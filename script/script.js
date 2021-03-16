128fde947770a13734a70c8af0b13ba2e07d9b2f
GatienH <ghacc@protonmail.com>
2021-03-16 13:12:15 +0100

    refacto index structure to add HTML5 tags

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
 document.querySelector('#burger').onclick = function() {
-  const menu = document.querySelector("#menu");
-  menu.classList.remove("hidden");
+  const nav = document.querySelector("nav");
+  nav.classList.remove("hidden");
 
   const body = document.querySelector("body");
   body.classList.add("overflow-y-hiden");
 }
 
-document.querySelector('#menu-close').onclick = function() {
-  const menu = document.querySelector("#menu");
-  menu.classList.add("hidden");
+document.querySelector('#close').onclick = function() {
+  const nav = document.querySelector("nav");
+  nav.classList.add("hidden");
 
   const body = document.querySelector("body");
   body.classList.remove("overflow-y-hiden");
